ee14237047adacf732401657fe567eb14c7612e5
Chizh13 <antonchizhevich48@fmail.com>
2020-03-08 14:15:57 +0300

    fix: result

--- src/index.js
+++ src/index.js
@@ -38,8 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+    let decoded = String();
+    let result = String();
+
+    let size = 10;
+    
+
+    for (let i = 0; i < Math.ceil(arr.length / size); i++) {
+    let subarray = arr.slice((i * size), (i * size) + size).join('');
+    
+    if (subarray == '**********') {
+            result += ' ';
+        } else {
+            for (let k = 0; k < subarray.length; k += 2) {
+                switch (subarray.slice(k, k + 2)) {
+                    case '10':
+                        decoded += '.';
+                        break;
+                    case '11':
+                        decoded += '-';
+                        break;
+                    default:
+                        continue;
+                }
+            }
+            result += MORSE_TABLE[decoded];
+        }
+
+        decoded = '';
+    }
+    return result;
+    
 }
+    
 
 module.exports = {
     decode
